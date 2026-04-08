d99f9ece485c1d292fb0b983d88c17a50a579e4e
guru4ec-dev <guru4ec@gmail.com>
2026-04-09 02:05:39 +0530

    Update LoginTests.java

--- src/test/java/com/automation/tests/LoginTests.java
+++ src/test/java/com/automation/tests/LoginTests.java
@@ -1,5 +1,7 @@
 package com.automation.tests;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
