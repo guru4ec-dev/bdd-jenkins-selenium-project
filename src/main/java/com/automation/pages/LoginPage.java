10b594f74521a0925d9b8400f72b4774fe13b9eb
guru4ec-dev <guru4ec@gmail.com>
2026-04-08 16:44:33 +0530

    Create PageObject.java for LoginPage implemented with Page Object Model pattern

---
+++ src/main/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package com.automation.pages;
+
+public class LoginPage {
+    // Elements
+    private String usernameField = "#username";
+    private String passwordField = "#password";
+    private String loginButton = "#loginButton";
+
+    // Methods
+    public void enterUsername(String username) {
+        // Code to enter username
+    }
+
+    public void enterPassword(String password) {
+        // Code to enter password
+    }
+
+    public void clickLogin() {
+        // Code to click the login button
+    }
+}
