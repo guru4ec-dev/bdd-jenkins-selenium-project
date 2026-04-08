c14be56b48e7f4a7aafe247d3b3ae07d9a1cb2b3
guru4ec-dev <guru4ec@gmail.com>
2026-04-08 16:50:06 +0530

    Add ConfigReader class for reading configuration properties

---
+++ src/main/java/com/automation/utils/ConfigReader.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private Properties properties;
+
+    public ConfigReader(String filePath) {
+        properties = new Properties();
+        try (InputStream input = new FileInputStream(filePath)) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
